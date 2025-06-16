bfca6ccca4944daaa3ff28125282f43f376f5a11
Redjul2110 <julian.fischer1@t-online.de>
2025-06-16 16:34:40 +0200

    2

--- code.js
+++ code.js
@@ -1,5 +1,9 @@
-<button onclick="downloadFile()">Download Jumper.exe</button>
-<script>
+function showSection(section) {
+  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
+  document.getElementById(section).classList.add('active');
+  document.querySelectorAll('nav a').forEach(a => a.classList.remove('active'));
+  document.getElementById('nav-' + section).classList.add('active');
+}
 function downloadFile() {
   // Passe den Pfad ggf. an, wenn sich der Ordner oder Dateiname ändert!
   const fileUrl = 'game/Jumper.exe'; 
@@ -10,4 +14,3 @@ function downloadFile() {
   a.click();
   document.body.removeChild(a);
 }
-</script>
